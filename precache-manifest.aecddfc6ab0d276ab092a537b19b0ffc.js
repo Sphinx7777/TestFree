7332790ab0d5c9f142ef5b0bcc40c4e939f891f3
Sphinx7777 <spamoglot13@gmail.com>
2019-11-01 19:39:43 +0200

    Updates

---
+++ precache-manifest.aecddfc6ab0d276ab092a537b19b0ffc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98836294612be118e1c6dffa230ecb1f",
+    "url": "/testFree/index.html"
+  },
+  {
+    "revision": "68f4f9c2510d1ab8eeff",
+    "url": "/testFree/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "458cf53251a4b61b4d53",
+    "url": "/testFree/static/css/main.b3379adc.chunk.css"
+  },
+  {
+    "revision": "68f4f9c2510d1ab8eeff",
+    "url": "/testFree/static/js/2.478b58aa.chunk.js"
+  },
+  {
+    "revision": "458cf53251a4b61b4d53",
+    "url": "/testFree/static/js/main.290f2f46.chunk.js"
+  },
+  {
+    "revision": "03842324c1bd39a0ed26",
+    "url": "/testFree/static/js/runtime-main.22c705a1.js"
+  },
+  {
+    "revision": "8f01c40b16b5481fc0b35625f424c4f1",
+    "url": "/testFree/static/media/avatar.8f01c40b.ico"
+  },
+  {
+    "revision": "a9b30fd8f38a14cfdd5964e924dd0977",
+    "url": "/testFree/static/media/mail.a9b30fd8.ico"
+  },
+  {
+    "revision": "6706617f45a2940e5de1b3163c4b7e93",
+    "url": "/testFree/static/media/phone_2.6706617f.png"
+  },
+  {
+    "revision": "16aaa35a889aed85ce1843d7cbbec13d",
+    "url": "/testFree/static/media/plus.16aaa35a.ico"
+  }
+]);
